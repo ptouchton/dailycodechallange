b2b58c98ffc59c97bd7f1a00d5b109bc38c8e2ef
ptouchton <p_touchton@yahoo.com>
2020-08-07 08:52:15 -0700

    working first pass

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -21,15 +21,40 @@ export class HelloComponent implements OnInit {
     
     const solve = (address: number,n: number) => {
       
+      const oddArr: Array<number> = [];
+      const evenArr: Array<number> = [];
+
       console.log(`address: ${address}`);
       console.log(`number of houses: ${n}`);
       
       const res = address % 2;
+      if (res === 1){
+         let j = -2;
+         for(let i = 0; i < n; i++){
+           j = j+2;
+           oddArr.push(1 + j );
+           console.log(`odds: ${oddArr[i]}`); 
+         };
+
+         j = oddArr[n-1];
+         console.log(`j: ${j}`);
+         j = j+2;
+         for(let i = 0; i < n; i++){
+           j = j-2;
+           evenArr.push(1 + j );
+           console.log(`evens: ${evenArr[i]}`); 
+         };
+
+         const locAddr = oddArr.indexOf(address);
+         return evenArr[locAddr];
+      }
+
+
       console.log(`result: ${res}`);
       
     };
 
-    let me = solve(3,3);
+    let me = solve(3,6);
     console.log(`solve result: ${me}`);
 
   }
